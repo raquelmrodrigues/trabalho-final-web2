abeb54d3ef6278b43adce3f00449c526dbfc746b
thalyson <thalysonbruck@hotmail.com>
2023-09-09 14:13:14 -0300

    arrumando imports

--- src/app/funcionario/funcionario.module.ts
+++ src/app/funcionario/funcionario.module.ts
@@ -4,16 +4,13 @@ import { RouterModule } from '@angular/router';
 import { InicialFuncionarioComponent } from './inicial-funcionario/inicial-funcionario.component';
 import { ConfirmarRecolhimentoComponent } from './confirmar-recolhimento/confirmar-recolhimento.component';
 import { ListarPedidosComponent } from './listar-pedidos/listar-pedidos.component';
-import { FinalizarPedidoComponent } from './finalizar-pedido/finalizar-pedido.component';
 
 
 @NgModule({
   declarations: [
     InicialFuncionarioComponent,
     ConfirmarRecolhimentoComponent,
-    FinalizarPedidoComponent,
     ListarPedidosComponent,
-    FinalizarPedidoComponent
   ],
   imports: [
     CommonModule,
