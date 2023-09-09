64bbaeea0f458265d339bd5dd737bc3287901626
mb-giu <giulia_mariane@hotmail.com>
2023-09-09 01:59:46 -0300

    NÃO DAR MERGE NISSO primeira parte do crud funcionario

---
+++ src/app/funcionario/inserir-funcionario/inserir-funcionario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inserir-funcionario',
+  templateUrl: './inserir-funcionario.component.html',
+  styleUrls: ['./inserir-funcionario.component.css']
+})
+export class InserirFuncionarioComponent {
+
+}
---
+++ src/app/shared/models/funcionario.model.ts
@@ -0,0 +1,9 @@
+export class Funcionario {
+    constructor(
+        public id?: number,
+        public email?: string,
+        public nome?: string,
+        public dataDeNasc?: Date,
+        public senha?: string,
+    ){}
+}
