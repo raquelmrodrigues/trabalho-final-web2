081593ae88a7067c675f94298703dd2f1ef2b75e
Anita Teixeira <anitateixeira55@gmail.com>
2023-10-09 10:34:19 -0300

    implementação da pagina de orçamento, acrecimo da classe item-pedido, modificação da classe pedido

--- src/app/shared/models/pedido.ts
+++ src/app/shared/models/pedido.ts
@@ -1,10 +1,13 @@
 import { StatusPedido } from "./status-pedido";
+import { ItemPedido } from "./item-pedido";
 export class Pedido {
     constructor(
         
         public id: number,
         public date?: Date,
-        public status?: StatusPedido
+        public status?: StatusPedido,
+        public items?: ItemPedido[],
+        public valorTotal?: number
     ){
 
     }
