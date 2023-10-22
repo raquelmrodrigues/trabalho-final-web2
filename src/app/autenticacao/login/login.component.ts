06e29021f7322af93b26a198c92751594b4b32a2
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2023-10-22 15:49:42 -0300

    validação login e ajustes no front

--- src/app/autenticacao/login/login.component.ts
+++ src/app/autenticacao/login/login.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterLink ,Router, NavigationEnd } from '@angular/router';
 
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+
+      console.log('Formulário válido. Dados:', this.loginForm.value);
+    }
+  }
 }
+
